fcd511c91f5d1a82f2407136f1e6414136bf70c0
Haider Ali <haider@candacecrowe.com>
2025-06-11 14:04:45 -0400

    Refactor if else conditions

--- bragbook.php
+++ bragbook.php
@@ -31,41 +31,47 @@ require_once BB_PLUGIN_DIR_MAIN_PATH . 'include/class-sitemap.php';
 require_once BB_PLUGIN_DIR_MAIN_PATH . 'include/class-consultation.php';
 require_once BB_PLUGIN_DIR_MAIN_PATH . 'include/class-seo.php';
 require_once BB_PLUGIN_DIR_MAIN_PATH . 'include/class-api-settings.php';
-$theme_directory = get_template(); 
-$header_path = get_stylesheet_directory() . '/header.php'; 
-class mvpbrag {
+$theme_directory = get_template();
+$header_path = get_stylesheet_directory() . '/header.php';
+class mvpbrag
+{
 
-    public function __construct() {
-		add_action('init', [$this, 'bragbook_enqueue_jquery']);
-        add_action('admin_enqueue_scripts', [ $this, 'bragbook_enqueue_scripts' ]);
-        add_action('wp_enqueue_scripts', [ $this, 'bragbook_enqueue_scripts' ]);
-        
-        add_action('init', [ $this, 'init' ]);
-        add_filter('template_include', [ 'mvpbrag\Activator', 'include_template' ]);
+    public function __construct()
+    {
+        add_action('init', [$this, 'bragbook_enqueue_jquery']);
+        add_action('admin_enqueue_scripts', [$this, 'bragbook_enqueue_scripts']);
+        add_action('wp_enqueue_scripts', [$this, 'bragbook_enqueue_scripts']);
+
+        add_action('init', [$this, 'init']);
+        add_filter('template_include', ['mvpbrag\Activator', 'include_template']);
         new \mvpbrag\Ajax_Handler();
         new \mvpbrag\Sitemap();
         new \mvpbrag\Seo();
         new \mvpbrag\Consultation();
     }
 
-    public function bragbook_enqueue_scripts() {
+    public function bragbook_enqueue_scripts()
+    {
         \mvpbrag\Activator::bb_enqueue_scripts();
     }
-	public function bragbook_enqueue_jquery() {
+    public function bragbook_enqueue_jquery()
+    {
         \mvpbrag\Activator::bb_enqueue_jquery();
     }
 
-    public function init() {
+    public function init()
+    {
         \mvpbrag\Shortcode::register();
         \mvpbrag\Shortcode::custom_rewrite_rules();
         \mvpbrag\Shortcode::custom_rewrite_flush();
-        
+
     }
 
     /**
      * Update rewrite rules based on the page slug when the page is saved.
      */
-    public function update_rewrite_rules_on_save($post_id) {
+    public function update_rewrite_rules_on_save($post_id)
+    {
         if (get_post_type($post_id) !== 'page' || wp_is_post_revision($post_id)) {
             return;
         }
@@ -73,7 +79,7 @@ class mvpbrag {
         $page_slug = $post->post_name;
         update_option('bragbook_page_slug', $page_slug);
     }
-    
+
 }
 
 // Initialize the plugin
--- template/bb-consultation.php
+++ template/bb-consultation.php
@@ -4,32 +4,34 @@ Template Name: Consultation Page Template
 */
 ?>
 <div class="bb-container-main">
-<main class="bb-main">
-    <?php include plugin_dir_path(__FILE__) . 'sidebar-template.php'; ?> 
+    <main class="bb-main">
+        <?php include plugin_dir_path(__FILE__) . 'sidebar-template.php'; ?>
 
-    <div class="bb-content-area">
-        <div class="bb-filter-attic">
-            <button type="button" class="bb-sidebar-toggle">
-                <img src="<?php echo BB_PLUGIN_DIR_PATH; ?>assets/images/caret-right-sm.svg" alt="toggle sidebar">
-            </button>
-            <h2><span>Consultation Request</span></h2>
+        <div class="bb-content-area">
+            <div class="bb-filter-attic">
+                <button type="button" class="bb-sidebar-toggle">
+                    <img src="<?php echo BB_PLUGIN_DIR_PATH; ?>assets/images/caret-right-sm.svg" alt="toggle sidebar">
+                </button>
+                <h2><span>Consultation Request</span></h2>
+            </div>
+            <form class="bb-form bb-consultation-form" id="bb-consultation-form" method="post" action="">
+                <input class="bb-is-required" name="name" type="text" placeholder="Name*">
+                <span class="bb-is-required-msg">Name is required</span>
+                <input class="bb-is-required" name="email" type="email" placeholder="Email*">
+                <span class="bb-is-required-msg">Email is required</span>
+                <input class="bb-is-required" name="phone" type="tel" placeholder="Phone*">
+                <span class="bb-is-required-msg">Number is required</span>
+                <textarea rows="6" name="description" placeholder="How can we help?"></textarea>
+                <button type="submit" id="bb-consultation-form-submit" name="submit">Submit</button>
+            </form>
+            <div class="bb-is-required-success"></div>
         </div>
-        <form class="bb-form bb-consultation-form" id="bb-consultation-form" method="post" action="">
-            <input class="bb-is-required" name="name" type="text" placeholder="Name*">
-            <span class="bb-is-required-msg">Name is required</span>
-            <input class="bb-is-required" name="email" type="email" placeholder="Email*">
-            <span class="bb-is-required-msg">Email is required</span>
-            <input class="bb-is-required" name="phone" type="tel" placeholder="Phone*">
-            <span class="bb-is-required-msg">Number is required</span>
-            <textarea rows="6" name="description" placeholder="How can we help?"></textarea>
-            <button type="submit" id="bb-consultation-form-submit" name="submit">Submit</button>
-        </form>
-        <div class="bb-is-required-success"></div>
-    </div>
-    <div class="bb-bottom-bar">
-        <img src="<?php echo BB_PLUGIN_DIR_PATH; ?>assets/images/fav-logo.svg" alt="logo">
-        <p><span>Use the MyFavorites tool</span> to help communicate your specific goals. If a result speaks to you, tap the heart.</p>
-    </div>
-</main>
+        <div class="bb-bottom-bar">
+            <img src="<?php echo BB_PLUGIN_DIR_PATH; ?>assets/images/fav-logo.svg" alt="logo">
+            <p><span>Use the MyFavorites tool</span> to help communicate your specific goals. If a result speaks to you,
+                tap the heart.</p>
+        </div>
+    </main>
 </div>
 <?php
+get_footer();
